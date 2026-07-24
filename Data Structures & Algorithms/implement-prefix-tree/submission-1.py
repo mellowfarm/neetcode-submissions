b195ca18a0688baec636a5033b76dc4490573865
Nicha Ing See <166499582+mellowfarm@users.noreply.github.com>
2026-07-24 17:35:08 +0800

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,36 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for letter in word:
+            if letter not in curr:
+                curr[letter] = {}
+            curr = curr[letter]
+        curr["end"] = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        idx = 0
+        while idx < len(word):
+            if word[idx] not in curr:
+                return False
+            curr = curr[word[idx]]
+            idx += 1
+        if curr.get("end", False):
+            return True
+        else:
+            return False
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        idx = 0
+        while idx < len(prefix):
+            if prefix[idx] not in curr:
+                return False
+            curr = curr[prefix[idx]]
+            idx += 1
+        return True
+        
